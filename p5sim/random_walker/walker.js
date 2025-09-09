22995c0507bf6fe49cc79f823c5ac6e9e0332039
Ashry Ramadhan <ashry.ramadhan@webscientia.id>
2025-09-09 14:29:09 +0700

    update: walker.js

--- p5sim/random_walker/walker.js
+++ p5sim/random_walker/walker.js
@@ -15,4 +15,49 @@ class Walker {
         this.x += xstep;
         this.y += ystep;
     }
+
+    step2() {
+        let x_dist = mouseX - this.x;
+        let y_dist = mouseY - this.y;
+        let total_dist = x_dist + y_dist;
+        let x_prob = x_dist / total_dist;
+        let y_prob = y_dist / total_dist;
+
+        // left prob larger than 50 %
+        if (x_dist < 0) {
+            let r1 = random(1);
+            if (r1 <= 0.5) {
+                this.x--;
+            } else {
+                this.x += 0;
+            }
+        }
+
+        if (x_dist > 0) {
+            let r1 = random(1);
+            if (r1 <= 0.5) {
+                this.x++;
+            } else {
+                this.x += 0;
+            }
+        }
+
+        if (y_dist < 0) {
+            let r1 = random(1);
+            if (r1 <= 0.5) {
+                this.y--;
+            } else {
+                this.y += 0;
+            }
+        }
+
+        if (y_dist > 0) {
+            let r1 = random(1);
+            if (r1 <= 0.5) {
+                this.y++;
+            } else {
+                this.y += 0;
+            }
+        }
+    }
 }
