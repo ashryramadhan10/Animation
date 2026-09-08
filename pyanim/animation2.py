cafbb82e183f3332309eb7c19a0758b03678a2c2
Ashry Ramadhan <ashry.ramadhan@webscientia.id>
2026-09-08 18:10:28 +0700

    update: animation2.py

--- pyanim/animation2.py
+++ pyanim/animation2.py
@@ -5,20 +5,29 @@ import matplotlib.animation as animation
 np.set_printoptions(suppress=True)
 
 t0 = 0
-t_end = 16
+t_end = 8
 dt = 0.02
 t = np.arange(t0, t_end + dt, dt)
 
 # blue train
-f1 = 0.125 # Hz
+f1 = 1/8 # Hz
 A1 = 7 # m
-train_blue = A1 * np.sin(2 * np.pi* f1 * t)
+train_blue = A1 * np.sin(2 * np.pi * f1 * t)
 
 # Red train:
-f2=0.125 # [hz]
-A2=-7 # [m]
-train_red=A2*np.cos(2*np.pi*f2*t)
+f2 = 1/8 # [hz]
+A2 = -7 # [m]
+train_red = A2 * np.cos(2 * np.pi * f2 *t)
 
+# piecewise function
+y_i = 13
+y_i_ani = y_i * np.ones(len(t))
+
+GREEN_DELAY = 2.0
+car_green = y_i - 2 * (t - GREEN_DELAY)**2 # delay 2 seconds t -> (t - 2), y_i as shift to 13 up and initial height
+
+PURPLE_DELAY = 6.0
+car_purple = y_i - 2 * (t - PURPLE_DELAY)
 
 # Animation
 
@@ -29,23 +38,47 @@ def update_plot(num):
     X_blue.set_data(t[0:num], train_blue[0:num])
     X_red.set_data(t[0:num], train_red[0:num])
 
-    return X_blue, X_red,
+    if (t[num] >= GREEN_DELAY):
+        Y_green.set_data(t[int(GREEN_DELAY/dt):num], car_green[int(GREEN_DELAY/dt):num]) # the concept is simple for GREEN_DELAY/dt, while t = cumsum(dt)
+    else:
+        Y_green2.set_data(t[0:num], y_i_ani[0:num])
+
+    return X_blue, X_red, Y_green, Y_green2,
 
 fig = plt.figure(figsize=(16,9),dpi=120,facecolor=(0.8,0.8,0.8))
 gs = gridspec.GridSpec(2,2)
 
 ax0 = fig.add_subplot(gs[0,0],facecolor=(0.9,0.9,0.9))
-ax0.set_xlim(t0,t_end)
-ax0.set_ylim(-max(A1, A2)-1,max(A1,A2)+1)
-ax0.grid(True)
 
-X_blue, =ax0.plot([],[],'-b',linewidth=3,label='X_blue = '+str(A1)+'*sin(2π*'+str(f1)+'*t)')
+X_blue,=ax0.plot([],[],'-b',linewidth=3,label='X_blue = '+str(A1)+'*sin(2π*'+str(f1)+'*t)')
 X_red,=ax0.plot([],[],'-r',linewidth=3,label='X_red = '+str(A2)+'*cos(2π*'+str(f2)+'*t)')
+
+ax0.set_xlim(t0, t_end)
+ax0.set_ylim(-max(A1, A2)-1, max(A1,A2)+1)
+ax0.grid(True)
+ax0.set_xlabel('time [s]')
+ax0.set_ylabel('X [m]')
+ax0.spines['bottom'].set_position('center')
+ax0.xaxis.set_label_coords(0.5,0)
+ax0.legend(bbox_to_anchor=(1,1.2),fontsize='medium')
 
 ax1 = fig.add_subplot(gs[1,0],facecolor=(0.9,0.9,0.9))
 
+# piecewise function
+Y_green,=ax1.plot([],[],'g',linewidth=3)
+Y_green2,=ax1.plot([],[],'g',linewidth=3,alpha=1)
+
+plt.xlim(t0,t_end)
+plt.ylim(-2,y_i+1)
+plt.grid(True)
+plt.xlabel('time [s]')
+plt.ylabel('Y [m]')
+ax1.spines['bottom'].set_position(('data',0))
+# ax1.spines['left'].set_position(('data',5))
+ax1.xaxis.set_label_coords(0.5,0)
+
 ax3 = fig.add_subplot(gs[:,1],facecolor=(0.9,0.9,0.9))
 
-ani=animation.FuncAnimation(fig, update_plot, frames = frame_amount, interval = 20, repeat = False, blit = True)
+ani=animation.FuncAnimation(fig, update_plot, frames = frame_amount, interval = 20, repeat = True, blit = True)
 
 plt.show()
